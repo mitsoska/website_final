20b193e496dd4cc566c34c1241f886f08bb6c02d
Dimitris.Katiforis <pioyi@mail.com>
2024-03-18 20:57:07 +0200

    Added quotes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,66 @@
-<main>
-  <a href = "/"><h1 class="logo">Δημήτρης Κατηφόρης</h1></a>
+<script>
+  let quotes = ["Οι μοντέρνοι δυτικοί έχουν μάθει να αγνοούν την ιδέα της φυλετικής ιεραρχίας. Είναι σοκαρισμένοι από τους νόμους που απαγορεύουν στους μαύρους να περπατούν στους δρόμους των λευκών, ή να διαβάζουν στα σχολεία των λευκών, ή να εξυπηρετούνται σε νοσοκομεία των λευκών. Αλλά η ιεραρχία των πλουσίων και των φτωχών, που υποχρεώνει τους πλούσιους να ζουν σε διαφορετικές γειτονίες, πιο πολυτελείς, να διαβάζουν σε σχολεία διαφορετικά και πιο προνομιούχα, ή να θεραπεύονται σε ιδιωτικές κλινικές, καλύτερα εξοπλισμένες, φαίνεται τελείως δικαιολογημένη στα μάτια πολλών Αμερικανών και Ευρωπαίων"];
 
-<nav>
-  <div>
-    <a href = "/posts">Άρθρα</a><a href = "/about">Ποιός είμαι;</a><a href="/software">Χρήσιμο λογισμικό</a><a href="/chemistry">Χημεία</a>
+  let quote_desc = ["Harari Yuvah-Noah, Sapiens"]
+  let random_quote = Math.floor(Math.random() * quotes.length);
+</script>
+
+<div class="ultra-main">
+  <main>
+    <a href = "/"><h1 class="logo">Δημήτρης Κατηφόρης</h1></a>
+
+    <nav>
+      <div>
+        <a href = "/posts">Άρθρα</a><a href = "/about">Ποιός είμαι;</a><a href="/software">Χρήσιμο λογισμικό</a><a href="/chemistry">Χημεία</a>
+      </div>
+    </nav>
+
+    <slot />
+
+    <footer>
+      <p class="email">Δημήτρης Κατηφόρης: <span>dimkatif@chemist.com</span></p>
+      <a href="https://codeberg.org/Dimitris.Katiforis" target="_blank">Ο λογαριασμός μου στο codeberg</a>
+    </footer>
+  </main>
+  <div class="quote">
+    <div class="quote-card">
+      <p>{ quotes[random_quote] }</p>
+      <p class="quote-desc"> {quote_desc[random_quote]}</p>
+    </div>
   </div>
-</nav>
-
-<slot />
-
-<footer>
-  <p class="email">Δημήτρης Κατηφόρης: <span>dimkatif@chemist.com</span></p>
-  <a href="https://codeberg.org/Dimitris.Katiforis" target="_blank">Ο λογαριασμός μου στο codeberg</a>
-</footer>
-</main>
+</div>
 <style>
 
+  .quote-desc {
+    font-weight: 800;
+  }
+  
+  .ultra-main {
+    display: flex;
+    align-items: start;
+    justify-contents: center;
+    gap: 1.5em;
+    margin: 2em auto;
+    max-width: 1200px;
+  }
+  .quote {
+    background-image: url(/paper.jpg);
+    padding: 1em;
+    max-width: 300px;
+    box-shadow: inset 0 0 1em rgb(150, 150, 150);
+    background-size: 30em;
+    font-size: 0.8rem;
+  }
+  
   .email {
      color: black;
      font-size: 0.8em;
   }
   
   main {
-    margin: 0 22vw;
-    margin-top: 2em;
     background-image: url(/paper.jpg);
-    background-size: 30em;        
+    background-size: 30em;
+    flex: 1;
     box-shadow: inset 0 0 1em rgb(150, 150, 150);
   } 
 
